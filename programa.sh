cf2c132d9a16d5089c398475e813acde3111413d
Óscar <oscarvegaherrera59@gmail.com>
2017-10-11 10:57:05 +0200

    Se muestra la resta

--- programa.sh
+++ programa.sh
@@ -9,3 +9,4 @@ echo "1º número introducido --> " $num1
 echo "2º número introducido --> " $num2
 echo
 echo "El resultado de la suma es: " $((num1+num2))
+echo "El resultado de la resta es: " $((num1-num2))
