ee1d1bbced6a47248bf442376fd2b3fd7c2f3676
Óscar <oscarvegaherrera59@gmail.com>
2017-10-11 10:49:56 +0200

    Se crea programa.sh

---
+++ programa.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+echo Calculadora
+echo
+read -p "Escribe el 1º número: " num1
+read -p "Escribe el 2º número: " num2
+echo
+echo "1º número introducido --> " $num1
+echo "2º número introducido --> " $num2
